56fca8173ae50b0c574a690e8628babb79ee74d2
gay <113976105+Fimav2@users.noreply.github.com>
2023-02-05 20:56:59 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -2,6 +2,10 @@
 #define EASY_LIST_H_INCLUDED
 using namespace std;
 
-void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);//1
+
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2);//2
+
+ int itc_positive_list(const vector <int> &mass);//3
 
 #endif // EASY_LIST_H_INCLUDED

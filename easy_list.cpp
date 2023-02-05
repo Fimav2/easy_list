0b2ea4dacd946721a6b9d8bca91b4ac53d9dd4cf
gay <113976105+Fimav2@users.noreply.github.com>
2023-02-05 13:29:30 +0300

    Add files via upload

---
+++ easy_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "easy_list.h"
+using namespace std;
+
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
+
+    for(int i = 0; i < mass.size(); i ++){
+    if (i % 2 == 0)
+
+    mass2.push_back(mass[i]);
+
+
+}
+}
---
+++ easy_list.h
@@ -0,0 +1,7 @@
+#ifndef EASY_LIST_H_INCLUDED
+#define EASY_LIST_H_INCLUDED
+using namespace std;
+
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
+
+#endif // EASY_LIST_H_INCLUDED

84b8e0b05ed359481789d3e7c054f44612d1e20a
gay <113976105+Fimav2@users.noreply.github.com>
2023-02-06 22:33:44 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -1,14 +1,23 @@
 #ifndef EASY_LIST_H_INCLUDED
 #define EASY_LIST_H_INCLUDED
+#include <vector>
 using namespace std;
 
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);//1
 
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2);//2
 
- int itc_positive_list(const vector <int> &mass);//3
+int itc_positive_list(const vector <int> &mass);//3
 
- int itc_sl_list(const vector <int> &mass);//4
+int itc_sl_list(const vector <int> &mass);//4
+
+bool itc_same_parts_list(const vector <int> &mass);//5
+
+void itc_rev_list(vector <int> &mass);//6
+
+void itc_rev_par_list(vector <int> &mass);//7
+
+void itc_rshift_list(vector <int> &mass);//8
 
 
 

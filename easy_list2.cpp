149bf1b0f0a9f22cb959471afc30d395562fec05
gay <113976105+Fimav2@users.noreply.github.com>
2023-02-26 21:14:36 +0300

    Update easy_list2.cpp

--- easy_list2.cpp
+++ easy_list2.cpp
@@ -3,6 +3,18 @@
 #include "easy_list.h"
 using namespace std;
 
+void itc_rev_list(vector <int> &mass){//6
+
+
+    int c;
+    int N = mass.size();
+    for(int i = 0; i < N/2; i++){
+        c = mass[N - 1 -i];
+        mass[N -1 -i] = mass[i];
+        mass[i] = c;
+    }
+}
+
 void itc_rev_par_list(vector <int> &mass){//7
     int c;
     int N = mass.size();
@@ -24,16 +36,7 @@ void itc_rev_par_list(vector <int> &mass){//7
 
 
 
-void itc_rev_par_list(vector <int> &mass) {//7
-  int cetchik;
-  if (mass.size() > 0){
-    for (int i = 0; i < mass.size() - 1; i += 2) {
-      cetchik = mass[i];
-      mass[i] = mass[i + 1];
-      mass[i + 1] = cetchik;
-}
-}
-}
+
 
 void itc_rshift_list(vector <int> &mass){//8
      if(mass.size() == 0){
